8bbd36d25d8d13533585a0db80689083dd1b51e3
sppaskal <paskalevslavi@gmail.com>
2025-04-17 10:09:56 -0700

    Added app container to center pages
    - Added an app container div which centers pages and keeps them from being too stretched
    - Adjusted the Login page to work with new app container
    - Extracted custom logout button styling to its own LogoutButton.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-// src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import Home from './pages/Home.jsx'
+import './App.css'
 
 function App () {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/home' element={<Home />} />
-        {/* Add more routes here as needed */}
-      </Routes>
+      <div className='app-container'>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
