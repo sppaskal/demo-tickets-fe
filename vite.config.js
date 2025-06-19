66e8eaf5f0c17e9f0cdfd1c685defd218bf426b5
sppaskal <sppaskal@users.noreply.github.com>
2025-06-19 14:14:00 -0600

    Adding polling to Vite config
    Enabled polling in Vite config so that code changes are reflected live in the container during development

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    watch: {
+      usePolling: true, // Enable polling for file changes
+      interval: 100, // Check every 100ms
+    },
   },
 });
