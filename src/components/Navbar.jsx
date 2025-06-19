365c3d5c63dd91091eb5caa384e12c9d5242fc00
sppaskal <sppaskal@users.noreply.github.com>
2025-06-19 14:19:06 -0600

    Updating navbar to use link instead of href
    Changed to link over href to avoid page reload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
-import '../styles/Navbar.css'
-import LogoutButton from './LogoutButton.jsx'
+   import { Link } from 'react-router-dom';
+   import '../styles/Navbar.css';
+   import LogoutButton from './LogoutButton.jsx';
 
-export default function Navbar () {
-  return (
-    <nav className='navbar'>
-      <div className='navbar-container'>
-        <div className='navbar-logo'>DemoTickets</div>
-        <div className='navbar-actions'>
-          <ul className='navbar-links'>
-            <li><a href='/events'>Events</a></li>
-            <li><a href='/about'>About</a></li>
-            <li><a href='/contact'>Contact</a></li>
-          </ul>
-          <LogoutButton />
-        </div>
-      </div>
-    </nav>
-  )
-}
+   export default function Navbar() {
+     return (
+       <nav className='navbar'>
+         <div className='navbar-container'>
+           <div className='navbar-logo'>DemoTickets</div>
+           <div className='navbar-actions'>
+             <ul className='navbar-links'>
+               <li><Link to='/events'>Events</Link></li>
+               <li><Link to='/about'>About</Link></li>
+               <li><Link to='/contact'>Contact</Link></li>
+             </ul>
+             <LogoutButton />
+           </div>
+         </div>
+       </nav>
+     );
+   }
